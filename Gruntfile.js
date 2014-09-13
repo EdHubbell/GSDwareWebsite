5454a49b2055a7b8f0096b6c65177c3ad0c82940
Ed Hubbell <edhubbell@gmail.com>
2014-09-12 23:08:54 -0400

    grunt finally working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,64 @@
-// Gruntfile with the configuration of grunt-express and grunt-open. No livereload yet!
-module.exports = function(grunt) {
+'use strict';
  
-  // Load Grunt tasks declared in the package.json file
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+// thank you, https://gist.github.com/adion/7580522 
+
+var
+  LIVERELOAD_PORT = 35729,
+  lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT }),
+  mountFolder = function( connect, dir ) {
+    return connect.static(require('path').resolve(dir));
+  };
  
-  // Configure Grunt 
+module.exports = function( grunt ) {
+ 
+  // load all grunt tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
+ 
   grunt.initConfig({
- 
-    // grunt-express will serve the files from the folders listed in `bases`
-    // on specified `port` and `hostname`
-    express: {
-      all: {
-        options: {
-          port: 9000,
-          hostname: "0.0.0.0",
-          bases: [__dirname], // Replace with the directory you want the files served from
-                              // Make sure you don't use `.` or `..` in the path as Express
-                              // is likely to return 403 Forbidden responses if you do
-                              // http://stackoverflow.com/questions/14594121/express-res-sendfile-throwing-forbidden-error
-          livereload: true
-        }
-      }
-    },
- 
-    // grunt-watch will monitor the projects files
     watch: {
-      all: {
-        // Replace with whatever file you want to trigger the update from
-        // Either as a String for a single entry 
-        // or an Array of String for multiple entries
-        // You can use globing patterns like `css/**/*.css`
-        // See https://github.com/gruntjs/grunt-contrib-watch#files
-        files: 'index.html',
+      livereload: {
+        files: [
+          '{,*/}*.html',
+          'static/{,*/}*.{css,js,png,jpg,gif,svg}'
+        ],
+        tasks: ['jshint'],
         options: {
-          livereload: true
+          livereload: LIVERELOAD_PORT
         }
       }
     },
- 
-    // grunt-open will open your browser at the project's URL
+    connect: {
+      options: {
+        port: 9000,
+        hostname: '0.0.0.0'
+      },
+      livereload: {
+        options: {
+          middleware: function( connect ) {
+            return [
+              lrSnippet,
+              mountFolder(connect, './')
+            ];
+          }
+        }
+      }
+    },
     open: {
-      all: {
-        // Gets the port from the connect configuration
-        path: 'http://localhost:<%= express.all.options.port%>'
+      server: {
+        url: 'http://localhost:<%= connect.options.port %>'
       }
     }
   });
  
-  // Creates the `server` task
-  grunt.registerTask('server', [
-    'express',
-    'open',
-    'watch'
-  ]);
+  grunt.registerTask('server', function() {
+    grunt.task.run([
+      'connect:livereload',
+      'open',
+      'watch'
+    ]);
+  });
+ 
+  grunt.registerTask('default', [ 'server' ]);
 };
