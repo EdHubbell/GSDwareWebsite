b566b1e1a71a04082d1b2a9b3daa81ac5cab9477
Ed Hubbell <edhubbell@gmail.com>
2014-09-18 00:30:37 -0400

    trying shtml

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function( grunt ) {
     watch: {
       livereload: {
         files: [
-          '{,*/}*.html',
+          '{,*/}*.*html',
           'static/{,*/}*.{css,js,png,jpg,gif,svg}'
         ],
         tasks: ['jshint'],
