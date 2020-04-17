9ca7abd9391267cb79499bab3611c42749a0202c
EdHubbell <ed@gsdware.com>
2020-04-17 03:48:46 -0400

    more commits to get bake to work

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,28 +15,38 @@ module.exports = function( grunt ) {
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-open');
-  grunt.loadNpmTasks('grunt-ssi');
- 
+  grunt.loadNpmTasks( "grunt-bake" );
+
   grunt.initConfig({
-// what a pain in the ass redering server side includes was. Just to get these old grunt files working againg took hours.
-// Now what happens is that the ssi: command puts a modified .shtml file that should do what we want it to do. 
-    ssi: {
-      options: {
-        cache: 'all',
-        baseDir: '.',
+    bake: {
+      build: {
+        files: {
+            "404.html": "bake_html/404.html",
+            "about.html": "bake_html/about.html",
+            "burst.html": "bake_html/burst.html",
+            "capitalize.html": "bake_html/capitalize.html",
+            "cloud.html": "bake_html/cloud.html",
+            "contact.html": "bake_html/contact.html",
+            "contractDevelopment.html": "bake_html/contractDevelopment.html",
+            "dataVisibility.html": "bake_html/dataVisibility.html",
+            "faq.html": "bake_html/faq.html",
+            "index.html": "bake_html/index.html",
+            "iotTracking.html": "bake_html/iotTracking.html",
+            "mobileDevices.html": "bake_html/mobileDevices.html",
+            "productDevelopment.html": "bake_html/productDevelopment.html",
+            "projectManagement.html": "bake_html/projectManagement.html",
+            "relay.html": "bake_html/relay.html",
+            "siteInfo.html": "bake_html/siteInfo.html",
+            "team.html": "bake_html/team.html",
+            "technology.html": "bake_html/technology.html",
+            "windowsApps.html": "bake_html/windowsApps.html",
+        }
       },
-      target: {
-        files: [{
-          expand: true,
-          cwd: '.',
-          src: ['**/*.html'],
-          dest: '.tmp'
-        }],
-      }
     },
     watch: {
-      options: {
-        livereload: true,
+      bake: {
+        files: [ "bake_html/**" ],
+        tasks: "bake:build"
       },
     },
     connect: {
@@ -47,7 +57,7 @@ module.exports = function( grunt ) {
           base: {
             path: '.',
             options: {
-              index: '.tmp\\index.html',
+              index: 'index.html',
     //         redirect: false,
             },
           },
@@ -63,7 +73,7 @@ module.exports = function( grunt ) {
  
   grunt.registerTask('server', function() {
     grunt.task.run([
-      'ssi',
+      'bake:build',
       'connect:server',
       'open:file',
       'watch'
