b1d7799363c2001b555e0a69bf2f863f0e80de85
EdHubbell <ed@gsdware.com>
2020-04-17 02:28:17 -0400

    getting toward ssi solution

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,59 +3,69 @@
 // thank you, https://gist.github.com/adion/7580522 
 
 var
-  LIVERELOAD_PORT = 35729,
-  lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT }),
+//  LIVERELOAD_PORT = 35729,
+//  lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT }),
   mountFolder = function( connect, dir ) {
-    return connect.static(require('path').resolve(dir));
+    return serveStatic(require('path').resolve(dir));
   };
  
 module.exports = function( grunt ) {
  
   // load all grunt tasks
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-ssi');
  
   grunt.initConfig({
-    watch: {
-      livereload: {
-        files: [
-          '{,*/}*.*html',
-          'static/{,*/}*.{css,js,png,jpg,gif,svg}'
-        ],
-        tasks: ['jshint'],
-        options: {
-          livereload: LIVERELOAD_PORT
-        }
+// what a pain in the ass redering server side includes was. Just to get these old grunt files working againg took hours.
+// Now what happens is that the ssi: command puts a modified .shtml file that should do what we want it to do. 
+    ssi: {
+      options: {
+        cache: 'all',
+        baseDir: '.',
+      },
+      target: {
+        files: [{
+          expand: true,
+          cwd: '.',
+          src: ['**/*.html'],
+          dest: '.tmp'
+        }],
       }
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
     },
     connect: {
-      options: {
-        port: 9000,
-        hostname: '0.0.0.0'
-      },
-      livereload: {
+      server: {
         options: {
-          middleware: function( connect ) {
-            return [
-              lrSnippet,
-              mountFolder(connect, './')
-            ];
-          }
+          port: 9000,
+//          keepAlive: true,  
+          base: {
+            path: '.',
+            options: {
+              index: '.tmp\\index.html',
+    //         redirect: false,
+            },
+          },
         }
-      }
+      },     
     },
     open: {
-      server: {
-        url: 'http://localhost:<%= connect.options.port %>'
-      }
+      file : {
+        path : 'http://localhost:<%= connect.server.options.port %>'
+      },
     }    
   });
  
   grunt.registerTask('server', function() {
     grunt.task.run([
-      'connect:livereload',
-      'open',
+      'ssi',
+      'connect:server',
+      'open:file',
       'watch'
     ]);
   });
